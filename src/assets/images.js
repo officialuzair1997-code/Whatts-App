2e1cb4d3c9f791c0491877d7f608a4bad834511b
talha7867 <officaltalha786@gmail.com>
2025-09-19 14:49:28 +0500

    first commit

---
+++ src/assets/images.js
@@ -0,0 +1,42 @@
+
+import three from "./three.svg"
+import secrchi from "./secrchi.svg"
+import plus from "./plus.png"
+import plusbtn from "./plusbtn.svg"
+import heroicon from "./heroicon.svg"
+import herotwo from "./herotwo.svg"
+import mic from "./mic.svg"
+import document from "./document.svg"
+import photo from "./photo.svg"
+import camera from "./camera.svg"
+import headphone from "./headphone-1.svg"
+import contact from "./contact.svg"
+import poll from "./poll.svg"
+import sticker  from "./sticker.svg"
+import event  from "./event.svg"
+import close from "./close.svg"
+import sendms from "./sendms.svg"
+
+const images = {
+    three,
+    secrchi,
+    plus,
+    plusbtn,
+    heroicon,
+    herotwo,
+    mic,
+    document,
+    photo,
+    camera,
+    headphone,
+    contact,
+    poll,
+    sticker,
+    event,
+    close,
+    sendms,
+ 
+ 
+}
+
+export default images
---
+++ src/utils.js
@@ -0,0 +1,144 @@
+import images from './assets/images';
+export const dropdownmenuOne = [
+    {
+        "test": "New group",
+        "isLastLi": false,
+    },
+    {
+        "test": "Started messages",
+        "isLastLi": false,
+    },
+    {
+        "test": "Select chat",
+        "isLastLi": false,
+    },
+    {
+        "test": "Disappearing message",
+        "isLastLi": false,
+    },
+    {
+        "test": "Log out",
+        "isLastLi": false,
+    },
+    {
+        "test": "Get WhatsApp for Windows",
+        "isLastLi": true,
+    },
+
+
+
+
+]
+
+
+
+
+
+export const dropdownmenu = [
+    "Group info",
+    "Select messages",
+    "Mute notifications",
+    "Disappearing message",
+    "Add to favorites",
+    "Close chat",
+    "Clear chat",
+    "Exit group",
+]
+
+
+export const dropdownmenutwo =[
+
+{icon:images.document,
+label:"Document"
+},
+{icon:images.photo,
+label:"Photos & videos"
+},
+{icon:images.headphone,
+label:"Audio"
+},
+{icon:images.contact,
+label:"Contact"
+},
+{icon:images.poll,
+label:"Poll"
+},
+{icon:images.sticker,
+label:"New sticker"
+},
+{ icon: images.event,
+     label: "Event",
+      badge: "New" }
+
+
+
+
+
+]
+
+export const allUsers = [
+    {
+        id: 1,
+        name: "Meta Ai",
+        description: "FA (Faculty of Arts) ke baad IT (I)",
+        image: "/public/An-meta.png",
+        date: "14/08/1997",
+        message:[]
+    },
+    {
+        id: 2,
+        name: "Avisa",
+        description: "AI Assistant for coding & writing",
+        image: "/public/Ai.jpg",
+        date: "14/08/2025",
+        message:[]
+    },
+    {
+        id: 3,
+        name: "Ahmad",
+        description: "FA (Faculty of Arts) ke baad IT (I)",
+        image: "/public/allah.jpg",
+        date: "26/08/2025",
+        message:[]
+    },
+    {
+        id: 4,
+        name: "Ayyaz",
+        description: "FA (Faculty of Arts) ke baad IT (I)",
+        image: "public/comp5.jpg",
+        date: "26/08/2025",
+        message:[]
+    },
+    {
+        id: 5,
+        name: "GPT",
+        description: "FA (Faculty of Arts) ke baad IT (I)",
+        image: "/public/comp1 (1).jpg",
+        date: "14/08/1997",
+        message:[]
+    },
+    {
+        id: 6,
+        name: "DeepSeek",
+        description: "AI Assistant for coding & writing",
+        image: "public/comp1 (2).jpg",
+        date: "14/08/2025",
+        message:[]
+    },
+    {
+        id: 7,
+        name: "shah",
+        description: "FA (Faculty of Arts) ke baad IT (I)",
+        image: "public/comp1 (3).jpg",
+        date: "26/08/2025",
+        message:[]
+    },
+    {
+        id: 8,
+        name: "sabo",
+        description: "FA (Faculty of Arts) ke baad IT (I)",
+        image: "public/comp4.jpg",
+        date: "26/08/2025",
+        message:[]
+    }
+];
